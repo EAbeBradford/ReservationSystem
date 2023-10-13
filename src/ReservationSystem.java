7dbef9df894ef66db3293a751930f65d0270ee83
Mx. Bradford <emma_bradford@milton.com>
2023-10-13 13:26:56 -0400

    added add reservation method and a display reservations

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -3,12 +3,29 @@ public class ReservationSystem {
     public static void main(String[] args) {
        ReservationSystem jarvis = new ReservationSystem();
     }
-
     public ReservationSystem(){
         Reservation gavin = new Reservation();
         gavin.numPeople = 13;
 
         Reservation dog = new Reservation(4, 8.00, 1, 645);
         dog.print();
+
+        Reservation [] reservations = new Reservation[10];
+        reservations[0] = dog;
+
+        reservations[1] = gavin;
+
+        reservations[1].print();
+
+        Reservation cat = new Reservation(2, 8.00,1, 90 );
+
+        reservations[2] = new Reservation(2, 8.00,1, 90 );
+        reservations[2].print();
+        for(int x =3; x < 10; x++){
+            reservations[x] = new Reservation(7, 7.00, 2, 6);
+            reservations[x].print();
+        }
+
+
     }
 }
